48c5fb4766ce928d590fe10d3f7e4cbedaed1e01
channharith <channhariths@gmail.com>
2022-10-06 08:16:27 -0400

    visual calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import Calendar from './Calendar'
+import { useEffect, useState } from 'react'
+
+
 
 function App() {
+  // const api_key = process.env.REACT_APP_GIPHY_KEY
+  // const url = `https://api.thecatapi.com/v1/images/search?api_key=${api_key}`;
+  // const url = 'https://api.thecatapi.com/v1/images/search?api_key=live_37fHdgA0fShUu6KOYXr2SYAt38uuBEMuVlEHJk7H8m1HwfGlDbKQzmFHt1oYngqU'
+  // // console.log(api_key);
+  // const [cat, setCat] = useState({})
+  //it has to be Camel Case 
+  // console.log(url);
+  // function GetACat() {
+
+  //   fetch(url)
+	// 	.then((res) => {
+  //     setCat(res)
+
+  //     //setting cat to res
+	// 		return res.json();
+	// 	})
+	// 	.then((res) => {
+	// 		console.log(res);
+  //     console.log(cat.url);
+  //     //console logging cat to check information
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
+
+  // }
+
+  // useEffect (() => {
+  //   GetACat()
+  // },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-      </header>
+      <Calendar/>
+      {/* <img class='cat' src={cat.url[0]} alt="cat picture" /> */}
     </div>
   );
 }
---
+++ src/Calendar.jsx
@@ -0,0 +1,55 @@
+function Calendar(){
+    return(
+        <>
+    <h1>Dog Days Calendar for October</h1>
+    <h2>Sunday Monday Tuesday Wednesday Thursday Friday Saturday</h2>
+    <div class="calendar">
+
+        <div className="day" id="SU5">Sept 25</div>
+      <div className="day" id="M5">Sept 26</div>
+      <div className="day" id="T5">Sept 27</div>
+      <div className="day" id="W5">Sept 28</div>
+      <div className="day" id="TH5">Sept 29</div>
+      <div className="day" id="F5">Sept 30</div>
+
+
+      <div className="day" id="SA1">Day 1</div>
+      <div className="day" id="SU1">Day 2</div>
+      <div className="day" id="M1">Day 3</div>
+      <div className="day" id="T1">Day 4</div>
+      <div className="day" id="W1">Day 5</div>
+      <div className="day" id="TH1">Day 6</div>
+      <div className="day" id="F1">Day 7</div>
+
+      <div className="day" id="SA2">Day 8</div>
+      <div className="day" id="SU2">Day 9</div>
+      <div className="day" id="M2">Day 10</div>
+      <div className="day" id="T2">Day 11</div>
+      <div className="day" id="W2">Day 12</div>
+      <div className="day" id="TH2">Day 13</div>
+      <div className="day" id="F2">Day 14</div>
+
+      <div className="day" id="SA3">Day 15</div>
+      <div className="day" id="SU3">Day 16</div>
+      <div className="day" id="M3">Day 17</div>
+      <div className="day" id="T3">Day 18</div>
+      <div className="day" id="W3">Day 19</div>
+      <div className="day" id="TH3">Day 20</div>
+      <div className="day" id="F3">Day 21</div>
+
+      <div className="day" id="SA4">Day 22</div>
+      <div className="day" id="SU4">Day 23</div>
+      <div className="day" id="M4">Day 24</div>
+      <div className="day" id="T4">Day 25</div>
+      <div className="day" id="W4">Day 26</div>
+      <div className="day" id="TH4">Day 27</div>
+      <div className="day" id="F4">Day 28</div>
+
+      <div className="day" id="SA5">Day 29</div>
+    
+    </div>
+    </>
+    )
+}
+
+export default Calendar;
